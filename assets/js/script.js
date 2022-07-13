dc5aabab4d7e66daaf8b3d0479b730f658ac0c1d
Max Taylor <maxbransontaylor@gmail.com>
2022-07-12 23:34:17 -0400

    completed function that gets RAWG data for any desired genre

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+//format dates for RAWG url
+//only takes YYYY-MM-DD
+//arg checks for argument "last-week"  in getGames to set last weeks date
 var formatDates = function (arg) {
   var initial = new Date();
   if (arg == "last-week") {
@@ -9,14 +12,17 @@ var formatDates = function (arg) {
   } else {
     var date = initial;
   }
+  //converts possible D to DD
   var day = date.getDate();
   if (day < 10) {
     day = "" + 0 + day;
   }
+  //converts possible M to MM
   var month = date.getMonth();
   if (month < 10) {
     month = "" + 0 + month;
   }
+  //i miss moment.js
   var year = date.getFullYear();
   var formated = year + "-" + month + "-" + day;
   return formated;
@@ -24,6 +30,7 @@ var formatDates = function (arg) {
 var getGames = function (genre) {
   var lastWeek = formatDates("last-week");
   var today = formatDates();
+  //gets top rated games from the past week
   var apiUrl =
     "https://api.rawg.io/api/games?page_size=10&tags=" +
     genre +
@@ -32,5 +39,10 @@ var getGames = function (genre) {
     "," +
     today +
     "&ordering=-rating&key=d1ca06a37be445e396ab6a2c11ae8516";
-  console.log(apiUrl);
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
+getGames("horror");
