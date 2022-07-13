3545e36ce77ca2bdd9267277b478c36ea1b54dc7
Max Taylor <maxbransontaylor@gmail.com>
2022-07-12 23:22:24 -0400

    created function to format dates for RAWG api url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
+var formatDates = function (arg) {
+  var initial = new Date();
+  if (arg == "last-week") {
+    var date = new Date(
+      initial.getFullYear(),
+      initial.getMonth(),
+      initial.getDate() - 7
+    );
+  } else {
+    var date = initial;
+  }
+  var day = date.getDate();
+  if (day < 10) {
+    day = "" + 0 + day;
+  }
+  var month = date.getMonth();
+  if (month < 10) {
+    month = "" + 0 + month;
+  }
+  var year = date.getFullYear();
+  var formated = year + "-" + month + "-" + day;
+  return formated;
+};
 var getGames = function (genre) {
-  var date = new Date();
-  console.log(date);
+  var lastWeek = formatDates("last-week");
+  var today = formatDates();
   var apiUrl =
     "https://api.rawg.io/api/games?page_size=10&tags=" +
     genre +
@@ -9,4 +32,5 @@ var getGames = function (genre) {
     "," +
     today +
     "&ordering=-rating&key=d1ca06a37be445e396ab6a2c11ae8516";
+  console.log(apiUrl);
 };
